069bd6a76b57de0a45fdf78a85097181b2567e32
ZwellyTheKidd <zwelly.thekidd@outlook.com>
2024-02-08 10:35:16 +0200

    live map

--- map.js
+++ map.js
@@ -8,16 +8,28 @@ L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
 
 navigator.geolocation.watchPosition(success, error);
 
+let marker,circle,zoomed;
+
 function success(pos) {
 
     const lat = pos.coords.latitude;
     const lng = pos.coords.longitude;
     const accuracy = pos.coords.accuracy;
 
-    L.marker([lat,lng]).addTo(map);
-    L.circle([lat,lng],{ radius: accuracy}).addTo(map);
+    if(marker){
+        map.removeLayer(marker);
+        map.removeLayer(circle);
+    }
 
-    
+    marker = L.marker([lat,lng]).addTo(map);
+    circle= L.circle([lat,lng],{ radius: accuracy}).addTo(map);
+
+    if (!zoomed) {
+        zoomed=map.fitBounds(circle.getBounds());
+    }
+
+    map.setView([lat,lng]);
+  
 
 }
 
@@ -25,9 +37,6 @@ function error(err){
 
     if(err.code==1){
         alert('Please allow geolocation access');
-    }
-    else{
-        alert('Cannot get current location');
     }
 }
 
